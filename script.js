e690c2da70168eeec83d4275d445f4377099b363
arthurzin-zinn <arthurmoney21@gmail.com>
2025-12-16 20:46:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function procurar(entradaU, entradaS) {
+    fetch("../bancodedados/Usuarios.json").then(response => response.json()).then(data => {
+        //alert(JSON.stringify(data));
+        const Usuario = data.find((user) => user.usuario === entradaU && user.senha === entradaS);
+        //alert(JSON.stringify(Usuario));
+        if (Usuario == undefined){
+            alert("usuario e senha não encotrado")
+        } else {
+            window.location.href = './noticias.html';
+        }
+
+    })
+}
+
+function logar(){
+    user = document.querySelector("#usuario").value;
+    pass = document.querySelector("#senha").value;
+
+    procurar(user, pass);
+}
